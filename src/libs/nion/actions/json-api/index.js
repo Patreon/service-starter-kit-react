3a5cb1c4c56ddb90c1ba9d1fe55771488e16e8e8
David Kettler <21echoes@gmail.com>
2017-01-18 23:31:05 -0800

    bring over modern nion (won’t work until next commit, where we port jsonapi-suites over to it)

---
+++ src/libs/nion/actions/json-api/index.js
@@ -0,0 +1,90 @@
+import { CALL_API, getJSON } from 'redux-api-middleware';
+import parseJsonApiResponse from './parse-json-api-response';
+import { JsonApiError } from './parse-json-api-error';
+import get from 'lodash.get';
+
+import {
+    JSON_API_REQUEST,
+    JSON_API_SUCCESS,
+    JSON_API_FAILURE,
+    JSON_API_BOOTSTRAP
+} from '../types';
+
+const getJsonApiRequestTypes = (dataKey, meta = {}, promiseHandler) => {
+    return [{
+        type: JSON_API_REQUEST,
+        meta: { dataKey, ...meta }
+    }, {
+        type: JSON_API_SUCCESS,
+        meta: { dataKey, ...meta },
+        payload: (action, state, res) => {
+            return getJSON(res).then((json) => {
+                // Resolve the passed in promise, if supplied
+                if (promiseHandler && promiseHandler.resolve) {
+                    promiseHandler.resolve();
+                }
+
+                return json && parseJsonApiResponse(json, meta);
+            });
+        }
+    }, {
+        type: JSON_API_FAILURE,
+        meta: { dataKey, ...meta },
+        payload: (action, state, res) => {
+            // Mannually handle the error here, rather than passing off to error middleware
+            const error = new JsonApiError(res);
+
+            // Reject the passed in promise, if supplied. Note we only want to reject the promise if
+            // it's being used, ie, there exist _deferred methods (promise-polyfill specific) field.
+            // This is to avoid uncaught exceptions being raised in addition to the error being
+            // raised in middleware
+            if (get(promiseHandler, 'promise._deferreds.length')) {
+                if (promiseHandler && promiseHandler.reject) {
+                    promiseHandler.reject(error);
+                }
+            }
+            return error;
+        }
+    }];
+};
+
+export const requestJsonApi = (dataKey, request, meta, promiseHandler) => {
+    return {
+        [CALL_API]: {
+            types: getJsonApiRequestTypes(dataKey, meta, promiseHandler),
+            headers: { 'Content-Type': 'application/vnd.api+json' },
+            ...request
+        }
+    };
+};
+
+const fetchJsonApi = (method) => (dataKey, { endpoint, body, meta }, promiseHandler) => {
+    return requestJsonApi(dataKey, {
+        credentials: 'include',
+        body: JSON.stringify(body),
+        endpoint,
+        method
+    }, { ...meta, method, endpoint }, promiseHandler);
+};
+
+export const getJsonApi = fetchJsonApi('GET');
+export const postJsonApi = fetchJsonApi('POST');
+export const patchJsonApi = fetchJsonApi('PATCH');
+
+export const deleteJsonApi = (dataKey, ref, options, promiseHandler) => {
+    return fetchJsonApi('DELETE')(dataKey, {
+        ...options,
+        meta: {
+            ...options.meta,
+            refToDelete: ref
+        }
+    }, promiseHandler);
+};
+
+export const bootstrapJsonApi = ({ dataKey, data }) => {
+    return {
+        type: JSON_API_BOOTSTRAP,
+        meta: { dataKey },
+        payload: parseJsonApiResponse(data)
+    };
+};
--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -5,15 +5,6 @@ import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
 import auth from './auth';
 import {reducer as form} from 'redux-form';
-import {
-    widgetLoadSuite,
-    widgetListSuite,
-    widgetSearchSuite,
-    widgetEditSuite,
-    widgetCreateSuite,
-    widgetDeleteSuite,
-    reducer as widgetReducer,
-} from './widget';
 
 export const reducers = {
     routing: routerReducer,
@@ -25,16 +16,6 @@ export const reducers = {
     //     counter2: counter,
     //     counter3: counter
     // }),
-    suites: combineReducers({
-        [widgetLoadSuite.actionTypes.ROOT]: widgetLoadSuite.reducers,
-        [widgetListSuite.actionTypes.ROOT]: widgetListSuite.reducers,
-        [widgetSearchSuite.actionTypes.ROOT]: widgetSearchSuite.reducers,
-        [widgetEditSuite.actionTypes.ROOT]: widgetEditSuite.reducers,
-        [widgetCreateSuite.actionTypes.ROOT]: widgetCreateSuite.reducers,
-        [widgetDeleteSuite.actionTypes.ROOT]: widgetDeleteSuite.reducers,
-    }),
-    // Any reducers you want applied at the root level should be combined into one reducer under the _root key
-    _root: widgetReducer
 };
 
 export const combinedReducers = combineReducers(reducers);
